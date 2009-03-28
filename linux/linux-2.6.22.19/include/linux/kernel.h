36f03841b25104bfb6dc1e144bd953aa7e7017f6
Harvey Harrison <harvey.harrison@gmail.com>
2009-03-28 15:38:31 +0000

    kernel: remove HIPQUAD()
    [ Upstream 2c60b6885a ]

--- linux/linux-2.6.22.19/include/linux/kernel.h
+++ linux/linux-2.6.22.19/include/linux/kernel.h
@@ -390,18 +390,6 @@ static inline char *hex_byte_pack_upper(char *buf, u8 byte)
 	((unsigned char *)&addr)[3]
 #define NIPQUAD_FMT "%u.%u.%u.%u"
 
-#if defined(__LITTLE_ENDIAN)
-#define HIPQUAD(addr) \
-	((unsigned char *)&addr)[3], \
-	((unsigned char *)&addr)[2], \
-	((unsigned char *)&addr)[1], \
-	((unsigned char *)&addr)[0]
-#elif defined(__BIG_ENDIAN)
-#define HIPQUAD	NIPQUAD
-#else
-#error "Please fix asm/byteorder.h"
-#endif /* __LITTLE_ENDIAN */
-
 /*
  * min()/max() macros that also do
  * strict type-checking.. See the
