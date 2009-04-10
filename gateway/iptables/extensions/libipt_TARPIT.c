41a392c90b5ae3b6b0a4280aec4ed87c3be7e435
Leonid Lisovskiy <lly.dev@gmail.com>
2009-04-10 14:30:29 +0000

    iptables: Add ipt_TARPIT target

---
+++ gateway/iptables/extensions/libipt_TARPIT.c
@@ -0,0 +1,65 @@
+/* Shared library add-on to iptables for TARPIT support */
+#include <stdio.h>
+#include <getopt.h>
+#include <iptables.h>
+
+static void
+help(void)
+{
+	fputs(
+"TARPIT takes no options\n"
+"\n", stdout);
+}
+
+static struct option opts[] = {
+	{ 0 }
+};
+
+static void
+init(struct ipt_entry_target *t)
+{
+}
+
+static int
+parse(int c, char **argv, int invert, unsigned int *flags,
+      const void *entry,
+      struct ipt_entry_target **target)
+{
+	return 0;
+}
+
+static void final_check(unsigned int flags)
+{
+}
+
+static void
+print(const void *ip,
+      const struct ipt_entry_target *target,
+      int numeric)
+{
+}
+
+static void save(const void *ip, const struct ipt_entry_target *target)
+{
+}
+
+static
+struct xtables_target tarpit = {
+	.name		=     "TARPIT",
+	.family		=     NFPROTO_IPV4,
+	.version	=     XTABLES_VERSION,
+	.size		=     XT_ALIGN(0),
+	.userspacesize	=     XT_ALIGN(0),
+	.help		=     &help,
+	.init		=     &init,
+	.parse		=     &parse,
+	.final_check	=     &final_check,
+	.print		=     &print,
+	.save		=     &save,
+	.extra_opts	=     opts
+};
+
+void _init(void)
+{
+	xtables_register_target(&tarpit);
+}
