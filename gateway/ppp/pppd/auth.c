0c64c684f8bc3dceb2be0ac4f3fc1b3e939503aa
Vladislav Grishenko <themiron@mail.ru>
2009-09-06 17:41:51 +0000

    pppd: apply Alt Linux demand.patch
    110-alt-demand.patch commits:
     83bcfe5 pppd: demand patch from Alt Linux
     f596239 ppp: update to 2.4.7 and refresh patches

--- gateway/ppp/pppd/auth.c
+++ gateway/ppp/pppd/auth.c
@@ -553,6 +553,9 @@ link_required(unit)
 void start_link(unit)
     int unit;
 {
+     /* we are called via link_terminated, must be ignored */
+    if (phase == PHASE_DISCONNECT)
+	return;
     status = EXIT_CONNECT_FAILED;
     new_phase(PHASE_SERIALCONN);
 
@@ -662,6 +665,7 @@ link_terminated(unit)
 	the_channel->disconnect();
 	devfd = -1;
     }
+    /* not only disconnect, cleanup should also be called to close the devices */
     if (the_channel->cleanup)
 	(*the_channel->cleanup)();
 
