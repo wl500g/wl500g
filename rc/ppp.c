9aaa765c55712935955b96b0835d27c165e2f945
Leonid Lisovskiy <lly.dev@gmail.com>
2009-12-12 12:05:58 +0000

    rc: Apply 300-ppp-connected.patch

--- rc/ppp.c
+++ rc/ppp.c
@@ -93,7 +93,7 @@ ipup_main(int argc, char **argv)
 
 	wan_up(wan_ifname);
 
-	logmessage(nvram_safe_get("wan_proto_t"), "connect to ISP");
+	logmessage(nvram_safe_get("wan_proto_t"), "connected to ISP");
 	wanmessage("");
 
 	dprintf("done\n");
