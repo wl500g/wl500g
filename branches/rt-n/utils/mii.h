2b3d984c4328dafc9d6c5228ab51856d22884244
lly.dev <lly.dev@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2009-12-16 19:28:06 +0000

    [RT-N] mii-tool: Support of gigabit switches

--- branches/rt-n/utils/mii.h
+++ branches/rt-n/utils/mii.h
@@ -34,6 +34,7 @@ struct mii_data {
 #define  MII_BMCR_LOOPBACK	0x4000
 #define  MII_BMCR_100MBIT	0x2000
 #define  MII_BMCR_AN_ENA	0x1000
+#define  MII_BMCR_POWERDONW	0x0800
 #define  MII_BMCR_ISOLATE	0x0400
 #define  MII_BMCR_RESTART	0x0200
 #define  MII_BMCR_DUPLEX	0x0100
@@ -43,18 +44,21 @@ struct mii_data {
 /* Basic Mode Status Register */
 #define MII_BMSR		0x01
 #define  MII_BMSR_CAP_MASK	0xf800
-#define  MII_BMSR_100BASET4	0x8000
-#define  MII_BMSR_100BASETX_FD	0x4000
-#define  MII_BMSR_100BASETX_HD	0x2000
-#define  MII_BMSR_10BASET_FD	0x1000
-#define  MII_BMSR_10BASET_HD	0x0800
+#define  MII_BMSR_100BASET4	0x8000	/* Can do 100mbps, 4k packets  */
+#define  MII_BMSR_100BASETX_FD	0x4000	/* Can do 100mbps, full-duplex */
+#define  MII_BMSR_100BASETX_HD	0x2000	/* Can do 100mbps, half-duplex */
+#define  MII_BMSR_10BASET_FD	0x1000	/* Can do 10mbps, full-duplex  */
+#define  MII_BMSR_10BASET_HD	0x0800	/* Can do 10mbps, half-duplex  */
+#define  MII_BMSR_100FULL2	0x0400	/* Can do 100BASE-T2 FDX */
+#define  MII_BMSR_100HALF2	0x0200	/* Can do 100BASE-T2 HDX */
+#define  MII_BMSR_ESTATEN	0x0100  /* Extended Status in R15 */
 #define  MII_BMSR_NO_PREAMBLE	0x0040
-#define  MII_BMSR_AN_COMPLETE	0x0020
-#define  MII_BMSR_REMOTE_FAULT	0x0010
-#define  MII_BMSR_AN_ABLE	0x0008
-#define  MII_BMSR_LINK_VALID	0x0004
-#define  MII_BMSR_JABBER	0x0002
-#define  MII_BMSR_EXT_CAP	0x0001
+#define  MII_BMSR_AN_COMPLETE	0x0020	/* Auto-negotiation complete   */
+#define  MII_BMSR_REMOTE_FAULT	0x0010	/* Remote fault detected       */
+#define  MII_BMSR_AN_ABLE	0x0008	/* Able to do auto-negotiation */
+#define  MII_BMSR_LINK_VALID	0x0004	/* Link status                 */
+#define  MII_BMSR_JABBER	0x0002	/* Jabber detected             */
+#define  MII_BMSR_ERCAP		0x0001	/* Ext-reg capability          */
 
 #define MII_PHY_ID1		0x02
 #define MII_PHY_ID2		0x03
@@ -86,10 +90,15 @@ struct mii_data {
 
 /* Gigabit Registers */
 #define MII_CTRL1000		0x09
+#define   MII_AN2_UNKNOWN1	0x0800
+#define   MII_AN2_UNKNOWN2	0x0400
 #define   MII_AN2_1000FULL	0x0200
 #define   MII_AN2_1000HALF	0x0100
 
 #define MII_STAT1000		0x0a
+#define   MII_LPA2_UNKNOWN1	0x4000
+#define   MII_LPA2_1000LOCALOK	0x2000
+#define   MII_LPA2_1000REMRXOK	0x1000
 #define   MII_LPA2_1000FULL	0x0800
 #define   MII_LPA2_1000HALF	0x0400
 
@@ -100,6 +109,10 @@ struct mii_data {
 #define   MII_EST_1000XFULL	0x8000		/* 1000BASE-X full duplex capable */
 
 /* Last register we need for show_basic_mii() */
-#define MII_BASIC_MAX		(MII_STAT1000+1)
+#define MII_BASIC_MAX		MII_STAT1000
+
+#define MII_MAXPHYREG		32
+#define EPHY_NONE		31	/* nvram: no phy present at all */
+#define EPHY_NOREG		30	/* nvram: no local phy regs */
 
 #endif /* _LINUX_MII_H */
