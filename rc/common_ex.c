ddd99c467ea1dd1cc923d2bb88b84fe457db93a2
Leonid Lisovskiy <lly.dev@gmail.com>
2010-03-05 18:06:03 +0000

    [RT-N] Merge from trunk

--- rc/common_ex.c
+++ rc/common_ex.c
@@ -550,6 +550,14 @@ void convert_asus_values()
 	if ((nvram_match("ssh_enable", "1") && nvram_invmatch("recent_ssh_enable", "0")) ||
 	    (nvram_match("usb_ftpenable_x", "1") && nvram_invmatch("recent_ftp_enable", "0")))
 		eval("insmod", "ipt_recent");
+#if defined(__CONFIG_IPV6__) && !defined(LINUX26)
+	if (nvram_invmatch("ipv6_proto", ""))
+	{
+		eval("insmod", "ip6_conntrack");
+		eval("insmod", "ip6t_state");
+//		eval("insmod", "ip6t_TCPMSS");
+	}
+#endif
 
 	update_lan_status(1);
 
