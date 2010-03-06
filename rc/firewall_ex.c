c22ea8ac00050ffbde01fb26df33685d24b4ec10
Vladislav Grishenko <themiron@mail.ru>
2010-03-06 09:28:18 +0000

    rc: fixed huge packets pass. thnx MultiMan @ wl500g.info

--- rc/firewall_ex.c
+++ rc/firewall_ex.c
@@ -835,7 +835,7 @@ filter_setting(char *wan_if, char *wan_ip, char *lan_if, char *lan_ip, char *log
 	if (nvram_match("wan_proto", "pppoe") ||
 		nvram_match("wan_proto", "pptp") || nvram_match("wan_proto", "l2tp"))
 	{
-		fprintf(fp, "-A FORWARD -p tcp --syn -j TCPMSS --clamp-mss-to-pmtu\n");
+		fprintf(fp, "-A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\n");
 	}
 
 	/* Accept related connections, skip rest of checks */
@@ -1102,7 +1102,7 @@ filter_setting(char *wan_if, char *wan_ip, char *lan_if, char *lan_ip, char *log
 		fprintf(fp, "-A FORWARD -s ff00::/8 -j %s\n", logaccept);
 	/* Clamp TCP MSS to PMTU of WAN interface */
 //	if (nvram_match("ipv6_proto", "tun6in4") || nvram_match("ipv6_proto", "tun6to4")) {
-//		fprintf(fp, "-A FORWARD -p tcp --syn -j TCPMSS --clamp-mss-to-pmtu\n");
+//		fprintf(fp, "-A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\n");
 //	}
 	/* Accept related connections, skip rest of checks */
 	fprintf(fp, "-A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT\n");
