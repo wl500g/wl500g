108f3a10d9d077956fb18cc5d0bf43341be2369a
Leonid Lisovskiy <lly.dev@gmail.com>
2010-03-09 17:22:45 +0000

    rc: WiMAX - revert minor part of r1306, comments you can see (in Russian):   http://wl500g.info/showpost.php?p=188374&postcount=245

--- rc/rc.c
+++ rc/rc.c
@@ -532,7 +532,7 @@ set_wan0_vars(void)
 	} else {
 		nvram_set("wan0_ifname", nvram_get("wan_ifname"));
 		nvram_set("wan0_ifnames", nvram_get("wan_ifnames"));
-		nvram_set("wan0_metric", nvram_safe_get("wan_metric"));
+		nvram_set("wan0_priority", nvram_safe_get("wan_priority"));
 	}
 }
 
