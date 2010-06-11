302894d7d971a92a3147e30215e3e9de13d5d0b6
Leonid Lisovskiy <lly.dev@gmail.com>
2010-06-11 19:41:45 +0000

    kernel: fs/partitions: backports from upstream kernel 2.6
     kernel.org commits:
     b84d879639f83d35d3fcd909222522c928bf974b [PARTITION] MSDOS: Fix Sun num_partitions handling.
     6bcf19d02a5d7e627fa054f2f10e0a8d830df326 block: send disk "change" event for rescan_partitions()
     73f20e58b1d586e9f6d3ddc3aad872829aca7743 FAT_VALID_MEDIA(): remove pointless test
     f751aa125d1843ea4a9a264b451fd5b1639fab20 fat_valid_media() isn't for userspace
     0607fd02587a6b4b086dc746d63123c1f284db68 fat: detect media without partition table correctly
     ac0d86f5809598ddcd6bfa0ea8245ccc910e9eac block: sanitize invalid partition table entries
     02c33b123e59cab5771e52a012aeb810500260a2 partitions: warn about the partition exceeding device capacity
     7d13af3279985f554784a45cc961f706dbcdbdd1 partitions: use sector size for EFI GPT
     87038c2d5bda2418fda8b1456a0ae81cc3ff5bd8 partitions: read whole sector with EFI GPT header
     8e0cc811e0f8029a7225372fb0951fab102c012f fs/partition/msdos: fix unusable extended partition for > 512B sector
     fa039d5f6b126fbd65eefa05db2f67e44df8f121 Validate size of EFI GUID partition entries.
     ea56505bedd03e21f497c59cece15a62b4398fc4 partitions/efi.c: replace useless kzalloc's by kmalloc's
    
    128-fs-partitions.patch commits:
     52089d1 [RT-N] kernel: Patch from Tomato project fs/partitions: backports from upstream kernel 2.6
     6c3025b kernel: fs/partitions: backports from upstream
     7cb2840 kernel: Validate size of EFI GUID partition entries.
     dc3b311 kernel: partitions/efi.c: replace useless kzalloc's by kmalloc's

--- linux/linux-2.6.22.19/fs/partitions/efi.c
+++ linux/linux-2.6.22.19/fs/partitions/efi.c
@@ -1,7 +1,9 @@
 /************************************************************
  * EFI GUID Partition Table handling
- * Per Intel EFI Specification v1.02
- * http://developer.intel.com/technology/efi/efi.htm
+ *
+ * http://www.uefi.org/specs/
+ * http://www.intel.com/technology/efi/
+ *
  * efi.[ch] by Matt Domsch <Matt_Domsch@dell.com>
  *   Copyright 2000,2001,2002,2004 Dell Inc.
  *
@@ -92,6 +94,7 @@
  *
  ************************************************************/
 #include <linux/crc32.h>
+#include <linux/math64.h>
 #include "check.h"
 #include "efi.h"
 
@@ -148,7 +151,8 @@ last_lba(struct block_device *bdev)
 {
 	if (!bdev || !bdev->bd_inode)
 		return 0;
-	return (bdev->bd_inode->i_size >> 9) - 1ULL;
+	return div_u64(bdev->bd_inode->i_size,
+		       bdev_hardsect_size(bdev)) - 1ULL;
 }
 
 static inline int
@@ -195,6 +199,7 @@ static size_t
 read_lba(struct block_device *bdev, u64 lba, u8 * buffer, size_t count)
 {
 	size_t totalreadcount = 0;
+	sector_t n = lba * (bdev_hardsect_size(bdev) >> 9);
 
 	if (!bdev || !buffer || lba > last_lba(bdev))
                 return 0;
@@ -202,7 +207,7 @@ read_lba(struct block_device *bdev, u64 lba, u8 * buffer, size_t count)
 	while (count) {
 		int copied = 512;
 		Sector sect;
-		unsigned char *data = read_dev_sector(bdev, lba++, &sect);
+		unsigned char *data = read_dev_sector(bdev, n++, &sect);
 		if (!data)
 			break;
 		if (copied > count)
@@ -237,7 +242,7 @@ alloc_read_gpt_entries(struct block_device *bdev, gpt_header *gpt)
                 le32_to_cpu(gpt->sizeof_partition_entry);
 	if (!count)
 		return NULL;
-	pte = kzalloc(count, GFP_KERNEL);
+	pte = kmalloc(count, GFP_KERNEL);
 	if (!pte)
 		return NULL;
 
@@ -264,15 +269,16 @@ static gpt_header *
 alloc_read_gpt_header(struct block_device *bdev, u64 lba)
 {
 	gpt_header *gpt;
+	unsigned ssz = bdev_hardsect_size(bdev);
+
 	if (!bdev)
 		return NULL;
 
-	gpt = kzalloc(sizeof (gpt_header), GFP_KERNEL);
+	gpt = kmalloc(ssz, GFP_KERNEL);
 	if (!gpt)
 		return NULL;
 
-	if (read_lba(bdev, lba, (u8 *) gpt,
-		     sizeof (gpt_header)) < sizeof (gpt_header)) {
+	if (read_lba(bdev, lba, (u8 *) gpt, ssz) < ssz) {
 		kfree(gpt);
                 gpt=NULL;
 		return NULL;
@@ -348,6 +354,12 @@ is_gpt_valid(struct block_device *bdev, u64 lba,
 		Dprintk("GPT: last_usable_lba incorrect: %lld > %lld\n",
 			(unsigned long long)le64_to_cpu((*gpt)->last_usable_lba),
 			(unsigned long long)lastlba);
+		goto fail;
+	}
+
+	/* Check that sizeof_partition_entry has the correct value */
+	if (le32_to_cpu((*gpt)->sizeof_partition_entry) != sizeof(gpt_entry)) {
+		pr_debug("GUID Partitition Entry Size check failed.\n");
 		goto fail;
 	}
 
@@ -609,6 +621,7 @@ efi_partition(struct parsed_partitions *state, struct block_device *bdev)
 	gpt_header *gpt = NULL;
 	gpt_entry *ptes = NULL;
 	u32 i;
+	unsigned ssz = bdev_hardsect_size(bdev) >> 9;
 
 	if (!find_valid_gpt(bdev, &gpt, &ptes) || !gpt || !ptes) {
 		kfree(gpt);
@@ -619,13 +632,14 @@ efi_partition(struct parsed_partitions *state, struct block_device *bdev)
 	Dprintk("GUID Partition Table is valid!  Yea!\n");
 
 	for (i = 0; i < le32_to_cpu(gpt->num_partition_entries) && i < state->limit-1; i++) {
+		u64 start = le64_to_cpu(ptes[i].starting_lba);
+		u64 size = le64_to_cpu(ptes[i].ending_lba) -
+			   le64_to_cpu(ptes[i].starting_lba) + 1ULL;
+
 		if (!is_pte_valid(&ptes[i], last_lba(bdev)))
 			continue;
 
-		put_partition(state, i+1, le64_to_cpu(ptes[i].starting_lba),
-				 (le64_to_cpu(ptes[i].ending_lba) -
-                                  le64_to_cpu(ptes[i].starting_lba) +
-				  1ULL));
+		put_partition(state, i+1, start * ssz, size * ssz);
 
 		/* If this is a RAID volume, tell md */
 		if (!efi_guidcmp(ptes[i].partition_type_guid,
