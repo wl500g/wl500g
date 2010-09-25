737d04861266e24ebefdb22ed7e453e6f26cf010
Fedor <fedork@ubuntu.(none)>
2010-09-25 05:02:30 -0400

    kernel: signalfd simplification
    Tomato-RT 972cf9be
    
    kernel.org commits
     abd96ecb298675a21c412a29f5de2f80174d5f18 exec: kill unsafe BUG_ON(sig->count) checks
     b8fceee17a310f189188599a8fa5e9beaff57eb0 [PATCH] signalfd simplification

--- linux/linux-2.6.22.19/kernel/fork.c
+++ linux/linux-2.6.22.19/kernel/fork.c
@@ -1442,7 +1442,7 @@ static void sighand_ctor(void *data)
 	struct sighand_struct *sighand = data;
 
 	spin_lock_init(&sighand->siglock);
-	INIT_LIST_HEAD(&sighand->signalfd_list);
+	init_waitqueue_head(&sighand->signalfd_wqh);
 }
 
 void __init proc_caches_init(void)
