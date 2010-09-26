f78815feb153de267f2b32ffd655ede6fa16c5f5
lly.dev <lly.dev@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2010-09-26 10:33:36 +0000

    [RT-N] Merged revisions 2124,2128-2131,2141,2145-2146,2150-2154 via svnmerge from /trunk
      r2141 | lly.dev | 2010-09-24 20:20:32 +0400
        busybox: Fixes from trunk
         sleep: fix fractional arguments in non-POSIX locale
         wget: flush output to network before receving reply
    
      r2152 | lly.dev | 2010-09-26 12:00:36 +0400
        busybox: ash: fix signal and "set -e" interaction
    
      r2153 | themiron@mail.ru | 2010-09-26 12:36:12 +0400
        rc: don't be too smart with non-rfc ISPs
    
      r2154 | themiron@mail.ru | 2010-09-26 13:23:00 +0400
        rc: fixed old staticroutes usage case

--- branches/rt-n/www/web_asus_en/quick.js
+++ branches/rt-n/www/web_asus_en/quick.js
@@ -283,24 +283,40 @@ function loadMode()
 
 	//alert(document.form.wan_dnsenable_x[0].checked);
 	//alert(document.form.wan_dnsenable_x[1].checked);
-	if (document.form.wan_nat_x.value == '1')
-	{    	
-		document.form.x_Mode[0].checked = true;
-		document.form.x_Mode[1].checked = false;    	
-		document.form.x_Mode[2].checked = false;    	    	    	    	  	       	    	    	    	
-	}
-	else if (document.form.wan_route_x.value == 'IP_Routed')
+	if (window.top.isModel() == "WL300")
 	{
-		document.form.x_Mode[0].checked = false;
-		document.form.x_Mode[1].checked = true;
-		document.form.x_Mode[2].checked = false;      	    	
+		if (document.form.wan_nat_x.value == '1')
+		{
+			document.form.x_Mode[0].checked = false;
+			document.form.x_Mode[1].checked = true;
+	        }
+		else
+		{
+			document.form.x_Mode[0].checked = true;
+			document.form.x_Mode[1].checked = false;
+		}
 	}
 	else
 	{
-		document.form.x_Mode[0].checked = false;
-		document.form.x_Mode[1].checked = false;
-		document.form.x_Mode[2].checked = true;  
-	}                                      
+		if (document.form.wan_nat_x.value == '1')
+		{
+			document.form.x_Mode[0].checked = true;
+			document.form.x_Mode[1].checked = false;
+			document.form.x_Mode[2].checked = false;
+		}
+		else if (document.form.wan_route_x.value == 'IP_Routed')
+		{
+			document.form.x_Mode[0].checked = false;
+			document.form.x_Mode[1].checked = true;
+			document.form.x_Mode[2].checked = false;
+		}
+		else
+		{
+			document.form.x_Mode[0].checked = false;
+			document.form.x_Mode[1].checked = false;
+			document.form.x_Mode[2].checked = true;  
+		}
+	}
 	//changeMode();    
 }
 
@@ -708,23 +724,41 @@ function saveMode(o)
 
 	document.form.next_page.value = 'Main_Operation.asp';
 
-	if (document.form.x_Mode[2].checked === true) //Access Point
-	{           	                
-		document.form.wan_nat_x.value = '0';
-		document.form.wan_proto.value = 'static';
-		document.form.wan_route_x.value = 'IP_Bridged';
-	}       
-	else if (document.form.x_Mode[1].checked === true) //Router
-	{              
-		document.form.wan_nat_x.value = '0';
-		document.form.wan_proto.value = 'static';
-		document.form.wan_route_x.value = 'IP_Routed';
-	}   
-	else if (document.form.x_Mode[0].checked === true) //Home Gateway
-	{             
-		document.form.wan_nat_x.value = '1';
-		document.form.wan_route_x.value = 'IP_Routed';       
-		document.form.wan_proto.value = 'pppoe';
+	if (window.top.isModel() == "WL300")
+	{
+		if (document.form.x_Mode[0].checked == true) //Access Point
+		{
+			document.form.wan_nat_x.value = '0';
+			document.form.wan_proto.value = 'static';
+			document.form.wan_route_x.value = 'IP_Bridged';
+		}
+		else if (document.form.x_Mode[1].checked == true) //Home Gateway
+		{
+			document.form.wan_nat_x.value = '1';
+			document.form.wan_route_x.value = 'IP_Routed';
+			document.form.wan_proto.value = 'pppoe';
+		}
+	}
+	else
+	{
+		if (document.form.x_Mode[2].checked === true) //Access Point
+		{
+			document.form.wan_nat_x.value = '0';
+			document.form.wan_proto.value = 'static';
+			document.form.wan_route_x.value = 'IP_Bridged';
+		}
+		else if (document.form.x_Mode[1].checked === true) //Router
+		{
+			document.form.wan_nat_x.value = '0';
+			document.form.wan_proto.value = 'static';
+			document.form.wan_route_x.value = 'IP_Routed';
+		}
+		else if (document.form.x_Mode[0].checked === true) //Home Gateway
+		{
+			document.form.wan_nat_x.value = '1';
+			document.form.wan_route_x.value = 'IP_Routed';
+			document.form.wan_proto.value = 'pppoe';
+		}
 	}
 	o.value = "  Save  ";	
 	document.form.action_mode.value = "  Save  ";
