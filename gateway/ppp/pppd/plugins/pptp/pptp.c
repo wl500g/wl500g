fd2ab7204d390daed8522b438cf4e436de9b83e3
Vladislav Grishenko <themiron@mail.ru>
2010-12-27 09:59:46 +0000

    pppd: pptp plugin: force ns refresh with pre-0.9.31 uclibc
    based on 430-uclibc-resolv.patch commits:
     7ee08a1 pptp/l2tp: force ns refresh with pre-0.9.31 uclibc
     e1cea4f [RT-N] Merged revisions 2447,2449-2452 via svnmerge from /trunk
        r2447 | lly.dev | 2010-12-25 01:26:34 +0500 usb_modeswitch: Minimize amount of printf() calls on help
        r2449 | rssdev10 | 2010-12-26 19:01:40 +0500 www: syntax and style enhancements contributed by Anatoly Sotskov
        r2450 | theMIROn | 2010-12-27 04:44:03 +0500 simplified some build targets
        r2451 | theMIROn | 2010-12-27 14:59:46 +0500 pptp/l2tp: force ns refresh with pre-0.9.31 uclibc
        r2452 | theMIROn | 2010-12-27 19:18:15 +0500 sshd: don't install script if there's no sshd
     257184d accel-pptp: peer route improvements

--- gateway/ppp/pppd/plugins/pptp/pptp.c
+++ gateway/ppp/pppd/plugins/pptp/pptp.c
@@ -54,6 +54,8 @@
 #include <stdlib.h>
 
 #include <net/route.h>
+#include <features.h>
+#include <resolv.h>
 
 extern char** environ;
 
@@ -130,6 +132,12 @@ static int pptp_start_client(void)
 	struct sockaddr_pppox src_addr,dst_addr;
 	struct hostent *hostinfo;
 
+#if !defined(__UCLIBC__) \
+ || (__UCLIBC_MAJOR__ == 0 \
+ && (__UCLIBC_MINOR__ < 9 || (__UCLIBC_MINOR__ == 9 && __UCLIBC_SUBLEVEL__ < 31)))
+	/* force ns refresh from resolv.conf with uClibc pre-0.9.31 */
+	res_init();
+#endif
 	hostinfo=gethostbyname(pptp_server);
   if (!hostinfo)
 	{
