e9af5baf4c6bf12ed6237069bfb31a10f240fca6
Jo-Philipp Wich <jow@openwrt.org>
2011-03-07 03:09:48 +0100

    pppd: Disable wtmp support
    Many uClibc based environments lack wtmp and utmp support, therfore remove
    the code updating the wtmp information.

--- gateway/ppp/pppd/sys-linux.c
+++ gateway/ppp/pppd/sys-linux.c
@@ -2172,6 +2172,7 @@ int ppp_available(void)
 
 void logwtmp (const char *line, const char *name, const char *host)
 {
+#if 0
     struct utmp ut, *utp;
     pid_t  mypid = getpid();
 #if __GLIBC__ < 2
@@ -2236,6 +2237,7 @@ void logwtmp (const char *line, const char *name, const char *host)
 
 	close (wtmp);
     }
+#endif
 #endif
 }
 #endif /* HAVE_LOGWTMP */
