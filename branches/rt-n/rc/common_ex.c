6ff33cb9d505628c1d7b6a1a35c386b256a122bb
themiron.ru <themiron.ru@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2011-03-19 16:18:40 +0000

    [RT-N] rc: fixed r2825 dumbness

--- branches/rt-n/rc/common_ex.c
+++ branches/rt-n/rc/common_ex.c
@@ -675,7 +675,7 @@ void convert_asus_values()
 
 #if defined(LINUX26) && defined(QOS)
 	/* Kinda smart fast nat management */
-	fputs_ex("/net/ipv4/netfilter/ip_conntrack_fastnat",
+	fputs_ex("/proc/sys/net/ipv4/netfilter/ip_conntrack_fastnat",
 	    (nvram_match("wan_nat_x", "1") && !nvram_match("qos_enable_x", "1") &&
 	    !nvram_match("misc_fastnat_x", "0")) ? "1" : "0");
 #endif
