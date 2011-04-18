f4cdcfdd6d57a026af56bd20ee11fbf98904509d
themiron.ru <themiron.ru@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2011-04-18 10:05:34 +0000

    Merged revisions 2900-2901 via svnmerge from /trunk ........   r2900 | themiron.ru | 2011-04-18 15:22:49
      rp-l2tp: workaround locally generated CCN
    ........
      r2901 | themiron.ru | 2011-04-18 15:51:28
      
      rc: don't deal with external ntpds
    ........

--- branches/rt-n/rc/services.c
+++ branches/rt-n/rc/services.c
@@ -357,9 +357,10 @@ stop_nas(void)
 int
 start_ntpc(void)
 {
-	pid_t pid;
+	pid_t pid = 0;
 	char *server0 = nvram_safe_get("ntp_server0"), *server1;
 	char *ntp_argv[] = {"ntpd", "-qt", "-p", server0, NULL, NULL, NULL};
+	FILE *fp;
 
 	if (strlen(server0) > 0) {
 		server1 = nvram_safe_get("ntp_server1");
@@ -368,6 +369,13 @@ start_ntpc(void)
 			ntp_argv[5] = server1;
 		}
 		_eval(ntp_argv, NULL, 0, &pid);
+
+		/* write pid */
+		if (pid && (fp = fopen("/var/run/ntpc.pid", "w")) != NULL)
+		{
+			fprintf(fp, "%d", pid);
+			fclose(fp);
+		}
 	}
 
 	return pid;
@@ -376,7 +384,9 @@ start_ntpc(void)
 int
 stop_ntpc(void)
 {
-	int ret = killall("ntpd", 0);
+	int ret = kill_pidfile("/var/run/ntpc.pid");
+	if (ret == 0)
+		unlink("/var/run/ntpc.pid");
 
 	return ret;
 }
