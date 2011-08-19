aa05cc48cb4603672805a4a6a52dace18d7ef448
Vladislav Grishenko <themiron@mail.ru>
2011-08-19 15:43:32 +0000

    [RT-N] rc: add dummy auth services

---
+++ rc/auth.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+
+#include <bcmnvram.h>
+#include <netconf.h>
+#include <shutils.h>
+#include "rc.h"
+
+int
+start_wpa_supplicant(void)
+{
+	return 0;
+}
+
+int
+stop_wpa_supplicant(void)
+{
+	return 0;
+}
+
+int
+start_telenet(void)
+{
+	return 0;
+}
+
+int
+stop_telenet(void)
+{
+	return 0;
+}
