2895dc973dfbb7977cfd06130e0f3e33cc6291bb
Vladislav Grishenko <themiron@mail.ru>
2011-08-20 19:26:06 +0000

    [RT-N] rc: 802.1x start/stop implementation

--- rc/auth.c
+++ rc/auth.c
@@ -16,13 +16,46 @@
 int
 start_wpa_supplicant(char *prefix)
 {
-	return 0;
+	FILE *fp;
+	char tmp[100];
+	char *options = "/etc/wpa_supplicant.conf";
+	char *wpa_argv[] = {
+	    "/usr/sbin/wpa_supplicant",
+	    "-B",
+	    "-D", "roboswitch", /* RT-N16? */
+	    "-i", nvram_safe_get(strcat_r(prefix, "ifname", tmp)),
+	    "-c", options,
+	    NULL
+	};
+
+	/* Generate options file */
+	if ((fp = fopen(options, "w")) == NULL) {
+		perror(options);
+		return -1;
+	}
+	fprintf(fp,
+		"ctrl_interface=/var/run/wpa_supplicant\n"
+		"ap_scan=0\n"
+		"fast_reauth=1\n"
+		"network={\n"
+		"	key_mgmt=IEEE8021X\n"
+		"	eap=MD5\n"
+		"	identity=\"%s\"\n"
+		"	password=\"%s\"\n"
+		"	eapol_flags=0\n"
+		"}\n",
+		nvram_safe_get(strcat_r(prefix, "pppoe_username", tmp)),
+		nvram_safe_get(strcat_r(prefix, "pppoe_passwd", tmp)));
+	fclose(fp);
+
+	/* Start wpa_supplicant */
+	return _eval(wpa_argv, NULL, 0, NULL);
 }
 
 int
 stop_wpa_supplicant(void)
 {
-	return 0;
+	return killall("wpa_supplicant", 0);
 }
 #endif
 
@@ -32,7 +65,7 @@ start_lanauth(char *prefix)
 {
 	char tmp[100];
 	char *lanauth_argv[] = {
-		"lanauth",
+		"/usr/sbin/lanauth",
 	    /*	"-v", protocol,	   */
 	    /*	"-l", acces level, */
 		"-p", nvram_safe_get(strcat_r(prefix, "pppoe_passwd", tmp)),
@@ -41,6 +74,7 @@ start_lanauth(char *prefix)
    		NULL
 	};
 
+	/* Start lanauth */
 	return _eval(lanauth_argv, NULL, 0, NULL);
 }
 
