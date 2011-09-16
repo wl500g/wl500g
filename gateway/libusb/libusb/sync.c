dd02e67b99b427b2ceded271d7a8a071db749ecb
Ludovic Rousseau <ludovic.rousseau+github@gmail.com>
2011-09-16 18:07:56 +0200

    libusb: Correctly handle LIBUSB_TRANSFER_OVERFLOW in libusb_control_transfer()
    sync.c: In function `libusb_control_transfer':
    sync.c:122: warning: enumeration value `LIBUSB_TRANSFER_OVERFLOW' not
    handled in switch
    
    Fixes #120.
    
    Upstream 1cc5b4a9

--- gateway/libusb/libusb/sync.c
+++ gateway/libusb/libusb/sync.c
@@ -132,6 +132,9 @@ API_EXPORTED int libusb_control_transfer(libusb_device_handle *dev_handle,
 	case LIBUSB_TRANSFER_NO_DEVICE:
 		r = LIBUSB_ERROR_NO_DEVICE;
 		break;
+	case LIBUSB_TRANSFER_OVERFLOW:
+		r = LIBUSB_ERROR_OVERFLOW;
+		break;
 	default:
 		usbi_warn(HANDLE_CTX(dev_handle),
 			"unrecognised status code %d", transfer->status);
