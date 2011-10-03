58cc09ef1f08a3260feaca47992468ffc3cc095a
themiron.ru <themiron.ru@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2011-10-03 12:39:22 +0000

    [SDK 5.60] rc: fix RT-N10U vlan map

--- branches/sdk560/rc/rc.c
+++ branches/sdk560/rc/rc.c
@@ -181,7 +181,6 @@ stb_set(void)
 			}
 		case MDL_RTN12:
 		case MDL_RTN10:
-		case MDL_RTN10U:
 		case MDL_WL500GPV2:
 			{
 			/* Set LAN ports */
@@ -209,6 +208,7 @@ stb_set(void)
 		case MDL_WL520GU:
 		case MDL_WL500GP:
 		case MDL_DIR320:
+		case MDL_RTN10U:
 			{
 			/* Set LAN ports */
 			char *vlan0ports[] = {
@@ -322,8 +322,14 @@ early_defaults(void)
 		{
 			if (!nvram_get("wan_ifname") || !nvram_get("vlan1hwname"))
 			{
-				nvram_set("vlan0ports", "0 1 2 3 5*");
-				nvram_set("vlan1ports", "4 5");
+				if (router_model == MDL_RTN10U)
+				{
+					nvram_set("vlan0ports", "1 2 3 4 5*");
+					nvram_set("vlan1ports", "0 5");
+				} else {
+					nvram_set("vlan0ports", "0 1 2 3 5*");
+					nvram_set("vlan1ports", "4 5");
+				}
 				nvram_set("vlan0hwname", "et0");
 				nvram_set("vlan1hwname", "et0");
 				nvram_set("landevs", "vlan0 wl0");
