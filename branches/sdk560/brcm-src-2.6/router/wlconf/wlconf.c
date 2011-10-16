0ff34ddfd7a423c71c785c9d5e6f4ceeb616a672
lly.dev <lly.dev@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2011-10-16 15:53:54 +0000

    [SDK 5.60] wlconf: Redo r2243

--- branches/sdk560/brcm-src-2.6/router/wlconf/wlconf.c
+++ branches/sdk560/brcm-src-2.6/router/wlconf/wlconf.c
@@ -1875,8 +1875,11 @@ wlconf(char *name)
 	 * For a STA - Always
 	 */
 	if (WLCONF_PHYTYPE_11N(phytype)) {
-		if (sta ||
-		    ((ap || apsta) && (nbw == WL_CHANSPEC_BW_40) && (bandtype == WLC_BAND_2G))) {
+		if (sta
+#ifdef I_AM_SURE_OBSS_IS_NOT_BROKEN
+		    || ((ap || apsta) && (nbw == WL_CHANSPEC_BW_40) && (bandtype == WLC_BAND_2G))
+#endif
+		) {
 			str = nvram_safe_default_get(strcat_r(prefix, "obss_coex", tmp));
 			obss_coex = atoi(str);
 		} else {
@@ -1915,8 +1918,7 @@ wlconf(char *name)
 #else
 		if (obss_coex || channel == 0) {
 			if (WLCONF_PHYTYPE_11N(phytype)) {
-				chanspec_t chanspec;
-				int pref_chspec;
+				chanspec_t chanspec = 0;
 
 				if (channel != 0) {
 					/* assumes that initial chanspec has been set earlier */
@@ -1927,14 +1929,14 @@ wlconf(char *name)
 					/* We're not doing auto-channel, give the driver
 					 * the preferred chanspec.
 					 */
-					WL_IOVAR_GETINT(name, "chanspec", &pref_chspec);
-					WL_IOVAR_SETINT(name, "pref_chanspec", pref_chspec);
+					WL_IOVAR_GETINT(name, "chanspec", (uint32 *)&chanspec);
+					WL_IOVAR_SETINT(name, "pref_chanspec", (uint32 )chanspec);
 				} else {
 					WL_IOVAR_SETINT(name, "pref_chanspec", 0);
 				}
 				chanspec = wlconf_auto_chanspec(name);
 				if (chanspec != 0)
-					WL_IOVAR_SETINT(name, "chanspec", chanspec);
+					WL_IOVAR_SETINT(name, "chanspec", (uint32 )chanspec);
 			} else {
 				/* select a channel */
 				val = wlconf_auto_channel(name);
