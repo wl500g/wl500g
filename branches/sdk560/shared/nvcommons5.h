67cf19e79a7cea4bf2c3838ee38426787ac05155
lly.dev <lly.dev@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2011-10-23 14:12:28 +0000

    SDK 5.60 branch reintegrated into trunk

--- branches/sdk560/shared/nvcommons5.h
+++
@@ -1,7 +0,0 @@
-#ifndef _NVCOMMONS5_H_
-#define _NVCOMMONS5_H_
-/* misc */
-#define STRSEP 	strsep_with_validation
-char * strsep_with_validation(char **ptr,char *sep,char * retchar);
-
-#endif /* _NVCOMMONS5_H_ */
