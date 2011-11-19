70a093e412105f7b4cf53e9cd4d8f472162c4398
Vladislav Grishenko <themiron@mail.ru>
2011-11-19 20:22:34 +0000

    ipv6: prefixed some variables, fixes dhcpv6 via ppp start

--- rc/network_ex.c
+++ rc/network_ex.c
@@ -142,7 +142,7 @@ int start_pppd(const char *prefix)
 #ifdef __CONFIG_IPV6__
 	/* Enable IPv6 and IPv6CP */
 	strcat_r(prefix, "ipv6_proto", tmp);
-	if ((nvram_match(tmp, "static") || nvram_match(tmp, "slaac") || nvram_match(tmp, "dhcpv6")) &&
+	if ((nvram_match(tmp, "static") || nvram_match(tmp, "slaac") || nvram_match(tmp, "dhcp6")) &&
 	    !nvram_invmatch(strcat_r(prefix, "ipv6_if_x", tmp), "0")) {
 		fprintf(fp, "+ipv6\n");
 	}
