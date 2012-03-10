4f27125919f4cbb0de8a3e0a564b665b923f8d55
Leonid Lisovskiy <lly.dev@gmail.com>
2012-03-10 13:32:09 +0000

    kernel: [NETFILTER]: xt_mark v1, xt_MARK v2. Merge xt_MARK into xt_mark
    629-xt_mark.patch commits:
     a34812a kernel: [NETFILTER]: xt_mark v1, xt_MARK v2. Merge xt_MARK into xt_mark
     e615f02 kernel: Fix apply r3943
     47b9641 kernel: netfilter: xtables: switch check functions to use bool

--- linux/linux-2.6.22.19/include/linux/netfilter/xt_mark.h
+++ linux/linux-2.6.22.19/include/linux/netfilter/xt_mark.h
@@ -1,9 +1,13 @@
 #ifndef _XT_MARK_H
 #define _XT_MARK_H
 
-struct xt_mark_info {
-    unsigned long mark, mask;
-    u_int8_t invert;
+struct xt_mark_tginfo2 {
+	u_int32_t mark, mask;
+};
+
+struct xt_mark_mtinfo1 {
+	u_int32_t mark, mask;
+	u_int8_t invert;
 };
 
 #endif /*_XT_MARK_H*/
