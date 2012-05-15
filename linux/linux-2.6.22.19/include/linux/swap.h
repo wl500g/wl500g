c4677c36304620e95b3bf52c7e7623ed5a7f36e2
Leonid Lisovskiy <lly.dev@gmail.com>
2012-05-15 17:42:48 +0000

    kernel: Decrease some hardcoded limits - max swapfiles 2, max inet protos 64

--- linux/linux-2.6.22.19/include/linux/swap.h
+++ linux/linux-2.6.22.19/include/linux/swap.h
@@ -31,7 +31,7 @@ static inline int current_is_kswapd(void)
  * on 32-bit-pgoff_t architectures.  And that assumes that the architecture packs
  * the type/offset into the pte as 5/27 as well.
  */
-#define MAX_SWAPFILES_SHIFT	5
+#define MAX_SWAPFILES_SHIFT	1
 #ifndef CONFIG_MIGRATION
 #define MAX_SWAPFILES		(1 << MAX_SWAPFILES_SHIFT)
 #else
