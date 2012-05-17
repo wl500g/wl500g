2f58df3666c443790b8a3e49ff3ef1e39f3c022d
themiron.ru <themiron.ru@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2012-05-17 12:21:35 +0000

    rc: conditional radvd usage

--- trunk/rc/services_ex.c
+++ trunk/rc/services_ex.c
@@ -262,7 +262,7 @@ int stop_dhcpd(void)
 }
 
 
-#ifdef __CONFIG_IPV6__
+#ifdef __CONFIG_RADVD__
 int
 start_radvd(void)
 {
@@ -393,7 +393,7 @@ stop_radvd(void)
 {
 	return killall("radvd");
 }
-#endif /* __CONFIG_IPV6__ */
+#endif /* __CONFIG_RADVD__ */
 
 int
 ddns_updated_main()
@@ -1956,8 +1956,10 @@ stop_service_main()
 	stop_igmpproxy();
 	stop_upnp();
 	stop_snmpd();
-#ifdef __CONFIG_IPV6__
+#ifdef __CONFIG_RADVD__
 	stop_radvd();
+#endif
+#ifdef __CONFIG_IPV6__
 	stop_dhcp6c();
 #endif
 	stop_dhcpd();
