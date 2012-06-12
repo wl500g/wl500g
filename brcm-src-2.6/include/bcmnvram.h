e17f7c511e5f774c2e9c49e7c2f039871614942a
Leonid Lisovskiy <lly.dev@gmail.com>
2012-06-12 20:05:48 +0000

    brcm sdk: Trivial cleanup

--- brcm-src-2.6/include/bcmnvram.h
+++ brcm-src-2.6/include/bcmnvram.h
@@ -150,7 +150,7 @@ extern int nvram_getall(char *nvram_buf, int count);
  * returns the crc value of the nvram
  * @param	nvh	nvram header pointer
  */
-uint8 nvram_calc_crc(struct nvram_header * nvh);
+uint8 nvram_calc_crc(const struct nvram_header * nvh);
 
 #endif /* _LANGUAGE_ASSEMBLY */
 
