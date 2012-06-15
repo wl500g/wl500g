0b57f96c1d22eaa801d55e61ba4c3da318ef8034
lly.dev <lly.dev@c3c545f6-d8d2-11dd-93da-dbb4cac96d7c>
2012-06-15 19:01:17 +0000

    httpd: Remove obsolete ralink support

--- trunk/httpd/httpd.h
+++ trunk/httpd/httpd.h
@@ -103,7 +103,6 @@ extern int ej_lan_leases(int eid, webs_t wp, int argc, char_t **argv);
 extern int ej_nat_table(int eid, webs_t wp, int argc, char_t **argv);
 extern int ej_route_table(int eid, webs_t wp, int argc, char_t **argv);
 extern int ej_wl_status(int eid, webs_t wp, int argc, char_t **argv);
-extern int ej_wl_status_nobr(int eid, webs_t wp, int argc, char_t **argv);
 
 extern int sys_renew(void);
 extern int sys_release(void);
