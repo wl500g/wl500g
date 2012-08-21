4a50c2c372cc55086e4a2187d92f131c1255dcc9
Eric Dumazet <edumazet@google.com>
2012-08-21 20:48:29 +0000

    kernel: ipv4: properly update pmtu
    Upstream 9b04f350
    
    Sylvain Munault reported following info :
    
     - TCP connection get "stuck" with data in send queue when doing
       "large" transfers ( like typing 'ps ax' on a ssh connection )
     - Only happens on path where the PMTU is lower than the MTU of
       the interface
     - Is not present right after boot, it only appears 10-20min after
       boot or so. (and that's inside the _same_ TCP connection, it works
       fine at first and then in the same ssh session, it'll get stuck)
     - Definitely seems related to fragments somehow since I see a router
       sending ICMP message saying fragmentation is needed.
     - Exact same setup works fine with kernel 3.5.1
    
    Problem happens when the 10 minutes (ip_rt_mtu_expires) expiration
    period is over.
    
    ip_rt_update_pmtu() calls dst_set_expires() to rearm a new expiration,
    but dst_set_expires() does nothing because dst.expires is already set.
    
    It seems we want to set the expires field to a new value, regardless
    of prior one.
    
    With help from Julian Anastasov.

--- linux/linux-2.6.22.19/net/ipv4/route.c
+++ linux/linux-2.6.22.19/net/ipv4/route.c
@@ -1579,7 +1579,7 @@ static void ip_rt_update_pmtu(struct dst_entry *dst, u32 mtu)
 			dst->metrics[RTAX_LOCK-1] |= (1 << RTAX_MTU);
 		}
 		dst->metrics[RTAX_MTU-1] = mtu;
-		dst_set_expires(dst, ip_rt_mtu_expires);
+		dst->expires = max(1UL, jiffies + ip_rt_mtu_expires);
 		call_netevent_notifiers(NETEVENT_PMTU_UPDATE, dst);
 	}
 }
