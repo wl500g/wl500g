ced5e80899a89f3e1e38def23c0031f51e5ac7fc
Vladislav Grishenko <themiron@mail.ru>
2015-04-21 10:36:57 +0500

    infosrv: add copyright notice

--- gateway/infosrv/infosrv.c
+++ gateway/infosrv/infosrv.c
@@ -1,4 +1,7 @@
 /*
+ * Simple ASUS Device Discovery daemon
+ * Copyright (c) 2012 Vladislav Grishenko <themiron@mail.ru>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
