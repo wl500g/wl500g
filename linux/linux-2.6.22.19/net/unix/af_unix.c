14d6f8a05cf9a74467d3e73473a338102d49c447
Mark Salyzyn <salyzyn@android.com>
2015-05-26 08:22:19 -0700

    kernel: unix: sk_socket can disappear when state is unlocked
    [ Upstream b48732e4a4 ]
    
    ----

--- linux/linux-2.6.22.19/net/unix/af_unix.c
+++ linux/linux-2.6.22.19/net/unix/af_unix.c
@@ -1772,6 +1772,10 @@ static long unix_stream_data_wait(struct sock * sk, long timeo)
 		unix_state_unlock(sk);
 		timeo = schedule_timeout(timeo);
 		unix_state_lock(sk);
+
+		if (sock_flag(sk, SOCK_DEAD))
+			break;
+
 		clear_bit(SOCK_ASYNC_WAITDATA, &sk->sk_socket->flags);
 	}
 
@@ -1835,6 +1839,10 @@ static int unix_stream_recvmsg(struct kiocb *iocb, struct socket *sock,
 		struct sk_buff *skb;
 
 		unix_state_lock(sk);
+		if (sock_flag(sk, SOCK_DEAD)) {
+			err = -ECONNRESET;
+			goto unlock;
+		}
 		skb = skb_peek(&sk->sk_receive_queue);
 		if (skb==NULL)
 		{
