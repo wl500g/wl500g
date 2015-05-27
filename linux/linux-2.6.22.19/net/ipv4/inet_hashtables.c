1d1c235f67c1c2dacb4d582b0875ec949eed7f80
Eric Dumazet <edumazet@google.com>
2015-05-27 10:46:02 -0700

    kernel: tcp: connect() from bound sockets can be faster
    [ Upstream e2baad9e4b ]

--- linux/linux-2.6.22.19/net/ipv4/inet_hashtables.c
+++ linux/linux-2.6.22.19/net/ipv4/inet_hashtables.c
@@ -300,9 +300,10 @@ not_unique:
 	return -EADDRNOTAVAIL;
 }
 
-static inline u32 inet_sk_port_offset(const struct sock *sk)
+static u32 inet_sk_port_offset(const struct sock *sk)
 {
 	const struct inet_sock *inet = inet_sk(sk);
+
 	return secure_ipv4_port_ephemeral(inet->rcv_saddr, inet->daddr,
 					  inet->dport);
 }
@@ -490,7 +491,11 @@ EXPORT_SYMBOL_GPL(__inet_hash_connect);
 int inet_hash_connect(struct inet_timewait_death_row *death_row,
 		      struct sock *sk)
 {
-	return __inet_hash_connect(death_row, sk, inet_sk_port_offset(sk),
+	u32 port_offset = 0;
+
+	if (!inet_sk(sk)->num)
+		port_offset = inet_sk_port_offset(sk);
+	return __inet_hash_connect(death_row, sk, port_offset,
 			__inet_check_established, __inet_hash_nolisten);
 }
 
