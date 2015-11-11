f47f64d5267fb8f48d5ac04c248c83240cbc18bb
Daniel Borkmann <daniel@iogearbox.net>
2015-11-11 23:25:43 +0100

    kernel: packet: infer protocol from ethernet header if unset
    [ Upstream c72219b75f ]

--- linux/linux-2.6.22.19/net/packet/af_packet.c
+++ linux/linux-2.6.22.19/net/packet/af_packet.c
@@ -84,6 +84,7 @@
 #ifdef CONFIG_INET
 #include <net/inet_common.h>
 #endif
+#include <linux/if_arp.h>
 
 /*
    Assumptions:
@@ -844,6 +845,15 @@ static void tpacket_destruct_skb(struct sk_buff *skb)
 	sock_wfree(skb);
 }
 
+static void tpacket_set_protocol(const struct net_device *dev,
+				 struct sk_buff *skb)
+{
+	if (dev->type == ARPHRD_ETHER) {
+		skb_reset_mac_header(skb);
+		skb->protocol = eth_hdr(skb)->h_proto;
+	}
+}
+
 static int tpacket_fill_skb(struct packet_sock *po, struct sk_buff * skb,
 		void * frame, struct net_device *dev, int size_max,
 		__be16 proto, unsigned char * addr)
@@ -904,6 +914,8 @@ static int tpacket_fill_skb(struct packet_sock *po, struct sk_buff * skb,
 				dev->hard_header_len);
 		if (unlikely(err))
 			return err;
+		if (!skb->protocol)
+			tpacket_set_protocol(dev, skb);
 
 		data += dev->hard_header_len;
 		to_write -= dev->hard_header_len;
