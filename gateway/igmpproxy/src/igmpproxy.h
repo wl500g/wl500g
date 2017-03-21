b799e46947f882e3a7ce07729dbb21a9af971bfc
Vladislav Grishenko <themiron@mail.ru>
2017-03-21 21:03:40 +0500

    igmpproxy: fix last memeber query interval to be 1sec per rfc2236
     this makes quickleave support not important in most cases

--- gateway/igmpproxy/src/igmpproxy.h
+++ gateway/igmpproxy/src/igmpproxy.h
@@ -126,7 +126,7 @@ void my_log( int Serverity, int Errno, const char *FmtSt, ... );
 // Define timer constants (in seconds...)
 #define INTERVAL_QUERY          125
 #define INTERVAL_QUERY_RESPONSE  10
-//#define INTERVAL_QUERY_RESPONSE  10
+#define INTERVAL_LASTMEMBER_QUERY 1
 
 #define ROUTESTATE_NOTJOINED            0   // The group corresponding to route is not joined
 #define ROUTESTATE_JOINED               1   // The group corresponding to route is joined
